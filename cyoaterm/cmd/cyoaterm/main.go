c808526508142a941ec7b4e51e4adf256bf756ab
apokryptein <6676998+apokryptein@users.noreply.github.com>
2024-07-21 19:29:12 -0400

    refactor

--- cyoaterm/cmd/cyoaterm/main.go
+++ cyoaterm/cmd/cyoaterm/main.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"html/template"
 	"os"
-	"os/exec"
-	"runtime"
 )
 
 func main() {
@@ -16,9 +14,9 @@ func main() {
 	fmt.Println()
 
 	// get flags
-	filename := flag.String("f", "gopher.json", "name of CYOA JSON file")
+	filename := flag.String("f", "../../configs/stories/gopher.json", "name of CYOA JSON file")
 	start := flag.String("s", "intro", "starting chapter of your story")
-	tmpl := flag.String("t", "../../template.out", "desired story template")
+	tmpl := flag.String("t", "../../configs/templates/default.tmpl", "desired story template")
 	flag.Parse()
 
 	// read in JSON file containing story data
@@ -31,44 +29,5 @@ func main() {
 	s, _ := cyoaterm.ParseJson(data)
 
 	t := template.Must(template.ParseFiles(*tmpl))
-	storyRepl(s, t, *start)
-}
-
-func storyRepl(s cyoaterm.Story, t *template.Template, chapter string) {
-	if chapter == "home" {
-		fmt.Println("we're going home")
-		execTemplate(s, t, chapter)
-		os.Exit(0)
-	}
-
-	execTemplate(s, t, chapter)
-
-	for {
-		fmt.Printf("Where are we going next: ")
-		var next int
-		fmt.Scanf("%d", &next)
-		storyRepl(s, t, s[chapter].Options[next].Arc)
-	}
-}
-
-func execTemplate(s cyoaterm.Story, t *template.Template, chapter string) {
-	ClearScreen()
-	err := t.Execute(os.Stdout, s[chapter])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "cyoaterm: error executing template: %v\n", err)
-	}
-}
-
-// Function taken from:
-// https://dev.to/muhammadsaim/simplifying-your-terminal-experience-with-go-clearing-the-screen-1p7f
-func ClearScreen() {
-	if runtime.GOOS == "windows" {
-		cmd := exec.Command("cmd", "/c", "cls")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	} else {
-		cmd := exec.Command("clear")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
+	cyoaterm.StoryRepl(s, t, *start)
 }
--- cyoaterm/story.go
+++ cyoaterm/story.go
@@ -3,6 +3,10 @@ package cyoaterm
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
+	"os"
+	"os/exec"
+	"runtime"
 )
 
 type Story map[string]Arc
@@ -29,5 +33,44 @@ func ParseJson(data []byte) (Story, error) {
 func (s Story) PrintArcTitles() {
 	for k := range s {
 		fmt.Printf("%s: %s\n", k, s[k].Title)
+	}
+}
+
+func StoryRepl(s Story, t *template.Template, chapter string) {
+	if chapter == "home" {
+		fmt.Println("we're going home")
+		execTemplate(s, t, chapter)
+		os.Exit(0)
+	}
+
+	execTemplate(s, t, chapter)
+
+	for {
+		fmt.Printf("Where are we going next: ")
+		var next int
+		fmt.Scanf("%d", &next)
+		StoryRepl(s, t, s[chapter].Options[next].Arc)
+	}
+}
+
+func execTemplate(s Story, t *template.Template, chapter string) {
+	clearScreen()
+	err := t.Execute(os.Stdout, s[chapter])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cyoaterm: error executing template: %v\n", err)
+	}
+}
+
+// Function taken from:
+// https://dev.to/muhammadsaim/simplifying-your-terminal-experience-with-go-clearing-the-screen-1p7f
+func clearScreen() {
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
 	}
 }
