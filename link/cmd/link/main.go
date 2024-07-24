5c4541eee3e2d08cfe5a42a934ffe3bf668b3652
apokryptein <6676998+apokryptein@users.noreply.github.com>
2024-07-23 21:57:16 -0400

    Refactored to parse.go
    Refactored main to parse.go to start implementing
    the code as a library. The Parse function will take an
    io.Reader to allow for use across various inputs: web, text, etc.

---
+++ link/cmd/link/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"link"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	// Obtain HTML file to parse from command line
+	filename := flag.String("f", "", "file containing HTML")
+	flag.Parse()
+
+	// Ensure a filename was provided
+	if len(os.Args) < 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// Read HTML file in as []byte
+	h, err := os.ReadFile(*filename)
+	if err != nil {
+		log.Fatalf("link: problem reading html file: %v", err)
+		os.Exit(1)
+	}
+
+	// Call function to parse Node Tree
+	links, _ := link.Parse(strings.NewReader(string(h)))
+
+	for _, link := range links {
+		fmt.Printf("HREF: %s\tTEXT: %s\n", link.Href, link.Text)
+	}
+}
