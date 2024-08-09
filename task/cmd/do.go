1ce753af8e148e81485d2fc97b341e671e7a337b
apokryptein <6676998+apokryptein@users.noreply.github.com>
2024-08-09 12:49:30 -0400

    feat(cmd/do.go): add support for completing multiple tasks
    added support for completing multiple tasks at once
    e.g. -> 'task do 1 2 3 4'

--- task/cmd/do.go
+++ task/cmd/do.go
@@ -13,16 +13,18 @@ func init() {
 	rootCmd.AddCommand(doCmd)
 }
 
-// TODO: add ability to "do" multiple tasks
-// e.g. -> task do 1 2 3
 var doCmd = &cobra.Command{
 	Use:   "do",
 	Short: "Mark task as done",
 	Run: func(cmd *cobra.Command, args []string) {
-		id, err := strconv.Atoi(args[0])
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "task: error parsing task ID: %v", err)
-			os.Exit(1)
+		var ids []int
+		for _, v := range args {
+			id, err := strconv.Atoi(v)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "task: error parsing task ID: %v", err)
+				os.Exit(1)
+			}
+			ids = append(ids, id)
 		}
 
 		tasks, err := db.ListTasks()
@@ -31,18 +33,20 @@ var doCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		if id < 1 || id > len(tasks) {
-			fmt.Fprint(os.Stderr, "task: invalid task ID")
-			os.Exit(1)
-		}
+		for _, id := range ids {
+			if id < 1 || id > len(tasks) {
+				fmt.Fprint(os.Stderr, "task: invalid task ID")
+				os.Exit(1)
+			}
 
-		taskId := tasks[id-1].Id
-		task, err := db.CompleteTask(taskId)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "task: error completing task in DB: %v", err)
-			os.Exit(1)
-		}
+			taskId := tasks[id-1].Id
+			task, err := db.CompleteTask(taskId)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "task: error completing task in DB: %v", err)
+				os.Exit(1)
+			}
 
-		fmt.Printf("You have completed the \"%s\" task.", task)
+			fmt.Printf("You have completed the \"%s\" task.\n", task)
+		}
 	},
 }
