404790f5151b6ee6b62be1cb80b84a42d7341b6f
apokryptein <6676998+apokryptein@users.noreply.github.com>
2024-09-09 14:05:28 -0400

    feat(renamer): add walk functionality
    added directory/file walk functionality with basic
    filename print

--- renamer/main.go
+++ renamer/main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
 )
+
+type FileEntry struct {
+	Info os.FileInfo
+	Path string
+}
 
 func main() {
 	fmt.Println("File renaming utility")
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "renamer: error getting pwd: %v", err)
+		os.Exit(1)
+	}
+
+	fileRoot := flag.String("r", pwd, "root directory to begin search")
+
+	var walkResults []FileEntry
+	err = filepath.WalkDir(*fileRoot, func(path string, entry os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		info, err := entry.Info()
+		if err != nil {
+			return err
+		}
+
+		walkResults = append(walkResults, FileEntry{Path: path, Info: info})
+		return nil
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "renamer: error walking directory: %v", err)
+		os.Exit(1)
+	}
+
+	for _, f := range walkResults {
+		fmt.Println(f.Path, f.Info.Name())
+	}
 }
