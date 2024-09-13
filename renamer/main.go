35edc0a01ee85437e5531ab6a60138cb6eaa7463
apokryptein <6676998+apokryptein@users.noreply.github.com>
2024-09-13 14:20:12 -0400

    feat(renamer): add test data & regex filename check
    Added a set of test data. Added functionality to check
    filename against specified regex. This is accomplished in
    the checkFileName() function

--- renamer/main.go
+++ renamer/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 )
 
 type FileEntry struct {
@@ -15,15 +16,20 @@ type FileEntry struct {
 func main() {
 	fmt.Println("File renaming utility")
 
+	// Get current directory to act as default
 	pwd, err := os.Getwd()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "renamer: error getting pwd: %v", err)
 		os.Exit(1)
 	}
 
+	// Define and parse CL flags
 	fileRoot := flag.String("r", pwd, "root directory to begin search")
+	regexPattern := flag.String("p", ".*_[0-9]{3}\\.[a-zA-Z]{3}$", "regex file matching pattern")
 	flag.Parse()
 
+	// Walk files/directories from fileRoot
+	// Append files to []FileEntry
 	var walkResults []FileEntry
 	err = filepath.WalkDir(*fileRoot, func(path string, entry os.DirEntry, err error) error {
 		if err != nil {
@@ -43,7 +49,19 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Functionality check for now:
+	// Iterate through results and check using regex
 	for _, f := range walkResults {
-		fmt.Println(f.Path, f.Info.Name())
+		// fmt.Println(f.Path, f.Info.Name())
+		if checkFilename(f.Info.Name(), *regexPattern) {
+			fmt.Println("Match: ", f.Info.Name())
+		}
 	}
+}
+
+// Check filename string against supplied regex
+func checkFilename(filename string, rx string) bool {
+	re := regexp.MustCompile(rx)
+	match := re.MatchString(filename)
+	return match
 }
